f6deebcbb3baf41e57478d2e288c0a43ce548d41
Salome-Serna-R <124018168+Salome-Serna-R@users.noreply.github.com>
2025-08-08 00:32:48 -0500

    Cleaning view.py code

--- pages/views.py
+++ pages/views.py
@@ -1,18 +1,22 @@
-from django.http import HttpResponseNotFound
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import TemplateView, ListView
-from django.views import View
-from django import forms
-from django.shortcuts import render, redirect
-from networkx import reverse
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import TemplateView, ListView, View
+from django.views.generic.edit import CreateView
+from django import forms
+from django.urls import reverse, reverse_lazy
+from django.core.validators import MinValueValidator
 from .models import Product
 
-# Create your views here.
+
+# Creación de constantes para no repetir strings
+TITLE_PRODUCTS = "Products - Online Store"
+SUBTITLE_PRODUCTS = "List of products"
+
+
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
@@ -26,7 +30,8 @@ class AboutPageView(TemplateView):
             "author": "Developed by: Your Name",
         })
         return context
-    
+
+
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
 
@@ -45,55 +50,55 @@ class ContactPageView(TemplateView):
 
 class ProductIndexView(View):
     template_name = 'products/index.html'
-    
+
     def get(self, request):
-        viewData = {}
-        viewData["title"] = "Products - Online Store"
-        viewData["subtitle"] = "List of products"
-        viewData["products"] = Product.objects.all()
-        return render(request, self.template_name, viewData)
-    
+        context = {
+            "title": TITLE_PRODUCTS,
+            "subtitle": SUBTITLE_PRODUCTS,
+            "products": Product.objects.all()
+        }
+        return render(request, self.template_name, context)
 
 
 class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
-        # Check if product id is valid
         try:
-            product_id = int(id)
-            if product_id < 1:
-                raise ValueError("Product id must be 1 or greater")
-            product = get_object_or_404(Product, pk=product_id)
-        except (ValueError, IndexError):
-            # If the product id is not valid, redirect to the home page
+            product = get_object_or_404(Product, pk=int(id))
+        except (ValueError, Product.DoesNotExist):
             return HttpResponseRedirect(reverse('home'))
 
-        viewData = {}
-        product = get_object_or_404(Product, pk=product_id)
-        viewData["title"] = product.name + " - Online Store"
-        viewData["subtitle"] = product.name + " - Product information"
-        viewData["product"] = product
-        return render(request, self.template_name, viewData)
+        context = {
+            "title": f"{product.name} - Online Store",
+            "subtitle": f"{product.name} - Product information",
+            "product": product
+        }
+        return render(request, self.template_name, context)
+
 
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
-    context_object_name = 'products'  # This will allow you to loop through 'products' in your template
+    context_object_name = 'products'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Products - Online Store'
-        context['subtitle'] = 'List of products'
+        context.update({
+            'title': TITLE_PRODUCTS,
+            'subtitle': SUBTITLE_PRODUCTS
+        })
         return context
-
-
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price']
+        # Validación directa en el form
+        validators = {
+            'price': [MinValueValidator(0.01)]
+        }
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -101,28 +106,14 @@ class ProductForm(forms.ModelForm):
             raise forms.ValidationError("Price must be greater than zero.")
         return price
 
-class ProductCreateView(View):
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
     template_name = 'products/create.html'
-    success_template_name = 'products/success.html'
+    success_url = reverse_lazy('product_list')
 
-    def get(self, request):
-        form = ProductForm()
-        viewData = {}
-        viewData["title"] = "Create product"
-        viewData["form"] = form
-        return render(request, self.template_name, viewData)
-
-    def post(self, request):
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            viewData = {
-                "title": "Product created",
-                "message": "Product created"
-            }
-            return render(request, self.success_template_name, viewData)
-        else:
-            viewData = {}
-            viewData["title"] = "Create product"
-            viewData["form"] = form
-            return render(request, self.template_name, viewData)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Create product"
+        return context
